927340355fbc43d5b2ebdad1a086b981812013d9
edwardgijswijt-byte <edwardgijswijt@gmail.com>
2025-10-20 16:19:24 +0200

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'system-ui', padding: 40 }}>
-      <h1>DreamJob Europe – coming soon!</h1>
-      <p>Drop a job URL and get your CV + cover + video in 8 s.</p>
-    </div>
-  );
-}
